d9636f1ae5969a6a2bcd6276e54597aaff7aaf08
Magdalena Martinez <orangeclever@gmail.com>
2020-08-13 19:42:57 -0700

    Redux Adding Favorites

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
 import { View, Text, ScrollView } from "react-native";
 import { Card } from "react-native-elements";
-import { connect } from 'react-redux';
-import { baseUrl } from '../shared/baseUrl';
+import { connect } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+import Loading from './LoadingComponent';
 
-const mapStateToProps = state => {
-  return{
-    campsites: state.campsite,
+const mapStateToProps = (state) => {
+  return {
+    campsites: state.campsites,
     promotions: state.promotions,
-    partners: state.partners
+    partners: state.partners,
+  };
+};
+function RenderItem(props) {
+  const {item} = props;
+
+  if(props.isLoading){
+    return <Loading />;
+  
   }
-}
-function RenderItem({ item }) {
+  if(props.errMess){
+    return(
+      <View>
+        <Text>{props.errMess}</Text>
+      </View>
+    )
+  }
+
   if (item) {
     return (
-      <Card
-        featuredTitle={item.name}
-        image={{uri: baseUrl + item.image}}>
-      
+      <Card featuredTitle={item.name} image={{ uri: baseUrl + item.image }}>
         <Text style={{ margin: 10 }}>{item.description}</Text>
       </Card>
     );
@@ -26,9 +38,6 @@ function RenderItem({ item }) {
 }
 
 class Home extends Component {
-
-  
-
   static navigationOptions = {
     title: "Home",
   };
@@ -37,11 +46,33 @@ class Home extends Component {
     return (
       <ScrollView>
         <RenderItem
-                    item={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]} />
-                <RenderItem
-                    item={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]} />
-                <RenderItem
-                    item={this.props.partners.partners.filter(partner => partner.featured)[0]} />
+          item={
+            this.props.campsites.campsites.filter(
+              (campsite) => campsite.featured
+            )[0]
+          }
+          isLoading={this.props.campsites.isLoading}
+          errMess={this.props.campsites.errMess}
+
+        />
+        <RenderItem
+          item={
+            this.props.promotions.promotions.filter(
+              (promotion) => promotion.featured
+            )[0]
+          }
+          isLoading={this.props.promotions.isLoading}
+          errMess={this.props.promotions.errMess}
+        />
+        <RenderItem
+          item={
+            this.props.partners.partners.filter(
+              (partner) => partner.featured
+            )[0]
+          }
+          isLoading={this.props.partners.isLoading}
+          errMess={this.props.partners.errMess}
+        />
       </ScrollView>
     );
   }
